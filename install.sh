96541f84b21db85dfab4c9a19ef40116b09ee242
jredders <jredders@users.noreply.github.com>
2021-03-31 23:41:40 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 cp nuvo.py /usr/local/bin/nuvo_server/
 cp nuvo_server.py /usr/local/bin/nuvo_server/
-cp zones_list.txt /usr/local/bin/nuvo_server/
-cp sources_list.txt /usr/local/bin/nuvo_server/
